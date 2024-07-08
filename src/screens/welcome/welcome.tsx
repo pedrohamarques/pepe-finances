8229cac83421f54033cc28934d77f5ed2edb936e
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-07-08 17:34:49 -0300

    feat: create welcome screen

---
+++ src/screens/welcome/welcome.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export function WelcomeScreen() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Welcome Screen</Text>
+    </View>
+  );
+}
