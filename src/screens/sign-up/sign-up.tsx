897f83e4c6cc33a4bd57a150cbd5e9815f7083e2
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-07-08 21:53:13 -0300

    feat: create sign up screen

---
+++ src/screens/sign-up/sign-up.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { ScreenContainer } from "@components/screen-container";
+
+export function SignUpScreen() {
+  return (
+    <ScreenContainer>
+      <Text>SignUp </Text>
+    </ScreenContainer>
+  );
+}
