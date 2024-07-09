ebe6d8f2b45f9ab13320b555961cbc21da421765
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-07-08 22:43:03 -0300

    feat: add loading state to custom button

--- src/components/button-custom/button-custom.tsx
+++ src/components/button-custom/button-custom.tsx
@@ -1,25 +1,44 @@
 import React from "react";
-import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import {
+  ActivityIndicator,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from "react-native";
 
 type CustomButtonProps = TouchableOpacityProps & {
   testID?: string;
   text: string;
+  isLoading?: boolean;
 };
 
 export function CustomButton({
   text,
   testID = "components.custom-button",
+  isLoading = false,
   ...rest
 }: CustomButtonProps) {
   return (
     <TouchableOpacity
       testID={testID}
+      disabled={isLoading}
       {...rest}
       className="bg-white p-2 rounded-md"
     >
-      <Text className="text-emerald-600 font-inter-semi-bold text-lg text-center tracking-wider">
-        {text}{" "}
-      </Text>
+      <View className="h-7 justify-center items-center">
+        {!isLoading ? (
+          <Text className="text-emerald-600 font-inter-semi-bold text-lg text-center tracking-wider">
+            {text}{" "}
+          </Text>
+        ) : (
+          <ActivityIndicator
+            testID="components.button-custom.activity-indicator"
+            size="small"
+            color="green"
+          />
+        )}
+      </View>
     </TouchableOpacity>
   );
 }
